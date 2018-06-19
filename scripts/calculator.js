79a63a1f31f66e9803455960444b5ccb4e691c9d
dev-welli <welli.abdullah@xero.com>
2018-06-19 17:02:58 +1200

    divide and multiply works!

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -14,6 +14,7 @@ function addEventListener(){
 }
 
 var tempDisplay = ''; // set up temp string for display
+var tempNum = ''; //
 
 /*
 https://stackoverflow.com/questions/50811591/button-id-cannot-ve-retrieved-shown-with-getelementbyid
@@ -34,43 +35,59 @@ function calculate(evt){
       keyValue = e.parentNode.id;
    }
  /*  
-1.kebalikannya if not isnan then calculate else show error?
-2. need to add multiply, divide, dot/decimal conversion
-3. need to add second variable for additional calculation? one for display and the other for storage number?
+1. Should I use !isNan -> calculate else show 'ERROR'?
+2. need to add multiply, divide, dot/decimal conversion DONE
+3. need to add second variable for additional calculation? one for display and the other for storage number? DONE
 4. need to figure out the plus minus functionality
 5. need to figure out how to limit input
 */
 
    if (keyValue === 'reset'){
       tempDisplay = '';
+      tempNum = '';
       document.getElementById("display").innerHTML = "0";
    }
    else if (keyValue === 'squareRoot'){
       if (isNaN(tempDisplay)){
          document.getElementById("display").innerHTML = "ERROR";
          tempDisplay = '';
+         tempNum = '';
       }
       else{
          document.getElementById("display").innerHTML = Math.sqrt(tempDisplay);
          tempDisplay = '';
+         tempNum = '';
       }
    }
    else if (keyValue === 'square'){
       if (isNaN(tempDisplay)){
          document.getElementById("display").innerHTML = "ERROR";
          tempDisplay = '';
+         tempNum = '';
       }
       else{
          document.getElementById("display").innerHTML = Math.pow(tempDisplay, 2);
          tempDisplay = '';
+         tempNum = '';
       }
    }
+   else if (keyValue === 'divide'){
+         tempDisplay += '÷';
+         tempNum += '/';
+         document.getElementById("display").innerHTML = tempDisplay;
+   }
+   else if (keyValue === 'multiply'){
+      tempDisplay += 'x';
+      tempNum += '*';
+      document.getElementById("display").innerHTML = tempDisplay;
+   }     
    else if (keyValue === 'equal'){
-         document.getElementById("display").innerHTML = eval(tempDisplay);
+         tempDisplay = document.getElementById("display").innerHTML = eval(tempNum);
          //tempDisplay = '';
    }
    else{
       tempDisplay += keyValue;
+      tempNum += keyValue;
       document.getElementById("display").innerHTML = tempDisplay;
    }
 }
