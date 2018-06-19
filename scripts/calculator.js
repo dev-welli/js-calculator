95459c26d707409c716da460f71ab90df0e9032c
dev-welli <welli.abdullah@xero.com>
2018-06-19 20:17:38 +1200

    notes on what to do

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -38,7 +38,7 @@ function calculate(evt){
 1. Should I use !isNan -> calculate else show 'ERROR'?
 2. need to add multiply, divide, dot/decimal conversion DONE
 3. need to add second variable for additional calculation? one for display and the other for storage number? DONE
-4. need to figure out the plus minus functionality
+4. need to figure out the plus minus functionality - https://stackoverflow.com/questions/4652104/convert-a-negative-number-to-a-positive-one-in-javascript/4652158
 5. need to figure out how to limit input -> if tempDisplay.length > 8 then pop?
 6. need to figure out 4 decimal points - http://www.jacklmoore.com/notes/rounding-in-javascript/
 */
